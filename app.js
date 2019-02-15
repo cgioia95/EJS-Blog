99caa6acf4d637de05f90088a5608cdebdaaf134
Christian Gioia <christiang101@gmail.com>
2019-02-15 15:48:21 +1100

    Compose functions, post view with paramater routing, read more function

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var _ =  require('lodash');
+
+const posts = [];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,8 +18,65 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// GETS
+
 app.get("/", function(req,res){
-  res.render("home", {homeInit: homeStartingContent});
+
+  res.render("home", {homeInit: homeStartingContent, posts: posts});
+
+});
+
+app.get("/about", function(req,res){
+  res.render("about", {aboutInit: aboutContent});
+});
+
+app.get("/contact", function(req,res){
+  res.render("contact", {contactInit: contactContent});
+});
+
+app.get("/compose", function(req,res){
+  res.render("compose", {contactInit: contactContent});
+});
+
+// + GET function that allows access to individual posts based on their title in the post view 
+
+app.get('/posts/:title', function (req, res) {
+
+  let reqTitle = _.lowerCase(req.params.title);
+  var match = false;
+
+  posts.forEach(function(post){
+    const displayTitle = post.title;
+    const displayContent = post.content;
+    const storedTitle = _.lowerCase(displayTitle);
+
+    if(storedTitle === reqTitle){
+      match = true;
+      console.log("Match Found!")
+    }
+    else{
+      console.log("No match");
+    }
+
+    if (match){
+    res.render("post", {header: displayTitle, content: displayContent});
+  }
+  });
+});
+
+// POSTS
+
+app.post("/compose", function(req,res){
+
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postContent
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
+
 });
 
 
